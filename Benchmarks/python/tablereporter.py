2e365c6fe6b88389b0eb17acbb206433b9b66430
domcha <dominic.charrier@googlemail.com>
2018-02-15 15:04:58 +0000

    Fix issues with y-axis scaling for linear plots.

--- Benchmarks/python/tablereporter.py
+++ Benchmarks/python/tablereporter.py
@@ -149,14 +149,16 @@ def createPlots():
                 axes.set_ylabel(yLabel)
                 if (yScale=="linear"):
                     axes.ticklabel_format(axis="y", style="sci", scilimits=(-2,2))
-                    axes.set_ylim([dataMin*0.95,dataMax*1.05])
+                    axes.set_ylim([0,dataMax*1.05])
+                else:
+                    yMin = 10**math.floor(math.log10(dataMin))
+                    yMax = max(dataMax*1.05,2.2*yMin)
+                    axes.set_ylim([yMin,yMax])
+                    
                 for i,x in enumerate(positions):
                     xTrans = ( xMargin + float(x) ) / (xLimits[1]-xLimits[0])
                     label  = labels[i]
                     axes.text(xTrans,0.05,"%s" % label,ha='center', va='bottom',fontweight="bold",fontsize=TINY_SIZE,rotation=90,transform=axes.transAxes)
-                    yMin = 10**math.floor(math.log10(dataMin))
-                    yMax = max(dataMax*1.05,2.2*yMin)
-                    axes.set_ylim([yMin,yMax])
                 
                 if not os.path.exists(plotFolderPath):
                     print("create directory "+plotFolderPath)
