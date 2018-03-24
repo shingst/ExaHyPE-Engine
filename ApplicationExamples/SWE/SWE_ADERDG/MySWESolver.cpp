dc1114e048bcf87ab96ba99b8c236755749edaf3
Stefan Haas <stefan.haas@tele2.at>
2018-03-24 08:48:06 +0100

    added b to variables, now working with wall boundaries

--- ApplicationExamples/SWE/SWE_ADERDG/MySWESolver.cpp
+++ ApplicationExamples/SWE/SWE_ADERDG/MySWESolver.cpp
@@ -16,7 +16,6 @@ using namespace kernels;
 
 const double grav = 9.81;
 
-
 tarch::logging::Log SWE::MySWESolver::_log( "SWE::MySWESolver" );
 
 
@@ -26,27 +25,33 @@ void SWE::MySWESolver::init(const std::vector<std::string>& cmdlineargs,const ex
 }
 
 void SWE::MySWESolver::adjustPointSolution(const double* const x,const double t,const double dt,double* Q) {
-  // Dimensions                        = 2
-  // Number of variables + parameters  = 3 + 1
-  if (tarch::la::equals(t,0.0)) {
+    // Dimensions                        = 2
+    // Number of variables + parameters  = 4 + 0
+
+    if (tarch::la::equals(t,0.0)) {
       initialData(x, Q);
-  }
+    }
 }
 
 void SWE::MySWESolver::boundaryValues(const double* const x,const double t,const double dt,const int faceIndex,const int normalNonZero,
-  const double * const fluxIn,const double* const stateIn,
-  double *fluxOut,double* stateOut) {
-  // Dimensions                        = 2
-  // Number of variables + parameters  = 3 + 1
+    const double * const fluxIn,const double* const stateIn,
+    double *fluxOut,double* stateOut) {
+    // Dimensions                        = 2
+    // Number of variables + parameters  = 4 + 0
 
-    stateOut[0] = stateIn[0];
-    stateOut[1] = stateIn[1];
-    stateOut[2] = stateIn[2];
+    //Outflow (not working)
+//    for(int i=0; i < NumberOfVariables; i++) {
+//        fluxOut[i] = fluxIn[i];
+//        stateOut[i] = stateIn[i];
+//    }
 
-
-    fluxOut[0] = fluxIn[0];
-    fluxOut[1] = fluxIn[1];
-    fluxOut[2] = fluxIn[2];
+    //Wall
+    std::copy_n(stateIn, NumberOfVariables, stateOut);
+    stateOut[1+normalNonZero] =  -stateOut[1+normalNonZero];
+    double _F[2][NumberOfVariables]={0.0};
+    double* F[2] = {_F[0], _F[1]};
+    flux(stateOut,F);
+    std::copy_n(F[normalNonZero], NumberOfVariables, fluxOut);
 }
 
 exahype::solvers::Solver::RefinementControl SWE::MySWESolver::refinementCriterion(const double* luh,const tarch::la::Vector<DIMENSIONS,double>& center,const tarch::la::Vector<DIMENSIONS,double>& dx,double t,const int level) {
@@ -62,51 +67,55 @@ exahype::solvers::Solver::RefinementControl SWE::MySWESolver::refinementCriterio
 
 
 void SWE::MySWESolver::eigenvalues(const double* const Q,const int d,double* lambda) {
-  // Dimensions                        = 2
-  // Number of variables + parameters  = 3 + 1
+    // Dimensions                        = 2
+    // Number of variables + parameters  = 4 + 0
 
-  ReadOnlyVariables vars(Q);
-  Variables eigs(lambda);
+    ReadOnlyVariables vars(Q);
+    Variables eigs(lambda);
 
-  const double c = std::sqrt(grav*vars.h());
-  const double ih = 1./vars.h();
-  double u_n = Q[d + 1] * ih;
+    const double c = std::sqrt(grav*vars.h());
+    const double ih = 1./vars.h();
+    double u_n = Q[d + 1] * ih;
 
-  eigs.h() = u_n + c;
-  eigs.hu() = u_n - c;
-  eigs.hv() = u_n;
+    eigs.h() = u_n + c;
+    eigs.hu() = u_n - c;
+    eigs.hv() = u_n;
+    eigs.b() = 0.0;
 }
 
 
 void SWE::MySWESolver::flux(const double* const Q,double** F) {
-  // Dimensions                        = 2
-  // Number of variables + parameters  = 3 + 1
+    // Dimensions                        = 2
+    // Number of variables + parameters  = 4 + 0
+  
+    ReadOnlyVariables vars(Q);
 
-  ReadOnlyVariables vars(Q);
+    const double ih = 1./vars.h();
 
-  const double ih = 1./vars.h();
+    double* f = F[0];
+    double* g = F[1];
 
-  double* f = F[0];
-  double* g = F[1];
+    f[0] = vars.hu();
+    f[1] = vars.hu()*vars.hu()*ih + 0.5*grav*vars.h()*vars.h();
+    f[2] = vars.hu()*vars.hv()*ih;
+    f[3] = 0.0;
 
-  f[0] = vars.hu();
-  f[1] = vars.hu()*vars.hu()*ih + 0.5*grav*vars.h()*vars.h();
-  f[2] = vars.hu()*vars.hv()*ih;
-
-  g[0] = vars.hv();
-  g[1] = vars.hu()*vars.hv()*ih;
-  g[2] = vars.hv()*vars.hv()*ih + 0.5*grav*vars.h()*vars.h();
+    g[0] = vars.hv();
+    g[1] = vars.hu()*vars.hv()*ih;
+    g[2] = vars.hv()*vars.hv()*ih + 0.5*grav*vars.h()*vars.h();
+    g[3] = 0.0;
   
 }
 
 
 
 void  SWE::MySWESolver::nonConservativeProduct(const double* const Q,const double* const gradQ,double* BgradQ) {
-    idx2 idx_gradQ(DIMENSIONS,NumberOfVariables + NumberOfParameters);
+    idx2 idx_gradQ(DIMENSIONS,NumberOfVariables);
 
     BgradQ[0] = 0.0;
     BgradQ[1] = grav*Q[0]*gradQ[idx_gradQ(0,3)];
     BgradQ[2] = grav*Q[0]*gradQ[idx_gradQ(1,3)];
+    BgradQ[3] = 0.0;
 }
 
 
