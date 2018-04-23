4f9b734170a460a68036e2f0b14317e58b7c55d6
Ben Hazelwood <benjamin.hazelwood@outlook.com>
2018-04-23 12:07:18 +0100

    Update Hamilton environment to xe_2018.2

--- Benchmarks/environment/hamilton/env-astro-nompi.sh
+++ Benchmarks/environment/hamilton/env-astro-nompi.sh
@@ -1,13 +1,14 @@
 module purge
-module load python
+module load python/3.6.3
 module load java
 module load slurm
-module load intel/xe_2017.2
-module load intelmpi/intel/2017.2
+module load intel/xe_2018.2
+module load intelmpi/intel/2018.2
 module load gcc/4.9.1
 module load gsl/intel
 
-export TBB_SHLIB="-L/ddn/apps/Cluster-Apps/intel/xe_2017.2/tbb/lib/intel64/gcc4.7 -ltbb"
+# Not required
+# export TBB_SHLIB="-L/ddn/apps/Cluster-Apps/intel/xe_2017.2/tbb/lib/intel64/gcc4.7 -ltbb"
 
 export EXAHYPE_CC=icpc
 
@@ -17,6 +18,3 @@ export MODE=Release
 export COMPILER=Intel
 export DISTRIBUTEDMEM=None
 export GPROF=off
-
-# optimised kernels
-export USE_IPO=on
