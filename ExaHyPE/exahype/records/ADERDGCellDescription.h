fb87eb7fb0447a0d46dc66dc99ad8a88b9567107
domcha <dominic.charrier@googlemail.com>
2018-05-02 17:05:41 +0100

    ADERDGCellDescription: Half size of MPI message.

--- ExaHyPE/exahype/records/ADERDGCellDescription.h
+++ ExaHyPE/exahype/records/ADERDGCellDescription.h
@@ -33,7 +33,7 @@ namespace exahype {
     *
     * 		   build date: 09-02-2014 14:40
     *
-    * @date   26/04/2018 15:28
+    * @date   02/05/2018 17:02
     */
    class exahype::records::ADERDGCellDescription { 
       
@@ -3063,7 +3063,7 @@ namespace exahype {
     *
     * 		   build date: 09-02-2014 14:40
     *
-    * @date   26/04/2018 15:28
+    * @date   02/05/2018 17:02
     */
    class exahype::records::ADERDGCellDescriptionPacked { 
       
@@ -6256,7 +6256,7 @@ namespace exahype {
        *
        * 		   build date: 09-02-2014 14:40
        *
-       * @date   26/04/2018 15:28
+       * @date   02/05/2018 17:02
        */
       class exahype::records::ADERDGCellDescription { 
          
@@ -9057,7 +9057,7 @@ namespace exahype {
        *
        * 		   build date: 09-02-2014 14:40
        *
-       * @date   26/04/2018 15:28
+       * @date   02/05/2018 17:02
        */
       class exahype::records::ADERDGCellDescriptionPacked { 
          
