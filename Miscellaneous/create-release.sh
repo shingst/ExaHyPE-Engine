da20bc37b195525b8ce8bfea9444d10afa381f36
Ben Hazelwood <benjamin.hazelwood@outlook.com>
2018-05-10 12:22:10 +0100

    prototype release script

---
+++ Miscellaneous/create-release.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then
+	>&2 echo "Please provide path to project folder(s)"
+	exit 1
+fi
+
+
+
+folders=(ExaHyPE Toolkit CodeGenerator Peano)
+folders=(${folders[*]/#/../} $@)
+files=(LICENSE.txt)
+files=(${files[*]/#/../})
+for i in ${folders[@]}; do
+	if [ ! -d "$i" ]; then
+		>&2 echo "Cannot find folder: $i"
+		exit 1
+	fi
+done
+
+for i in ${files[@]}; do
+	if [ ! -f "$i" ]; then
+		>&2 echo "Cannot find file: $i"
+		exit 1
+	fi
+done
+
+
+peanoVersion="Peano-$(svnversion ../Peano/peano)"
+exaVersion="ExaHyPE-$(git log --format="%h" -n 1)"
+tarName="$exaVersion-$peanoVersion.tar.gz"
+
+echo $tarName 
+
+tar --exclude-vcs -czvf  $tarName ${folders[*]} ${files[*]}
