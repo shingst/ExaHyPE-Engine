9f8eb7d00e860346281f1bd37517e4f6c5da70ac
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2018-06-20 11:00:15 +0100

    Remove warning.

--- ExaHyPE/exahype/solvers/ADERDGSolver.cpp
+++ ExaHyPE/exahype/solvers/ADERDGSolver.cpp
@@ -4887,7 +4887,6 @@ void exahype::solvers::ADERDGSolver::pullUnknownsFromByteStream(
   assertion(CompressionAccuracy>0.0);
 
   #if !defined(ValidateCompressedVsUncompressedData)
-  const int unknownsPerCell         = getUnknownsPerCell();
   const int dataPointsPerCell       = getDataPerCell();
   const int unknownsPerCellBoundary = getUnknownsPerCellBoundary();
 
