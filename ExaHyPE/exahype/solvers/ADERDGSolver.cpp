99778558a535c316df2fb045beafe738269309ac
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2018-06-20 10:37:03 +0100

    Fix another compiler error.

--- ExaHyPE/exahype/solvers/ADERDGSolver.cpp
+++ ExaHyPE/exahype/solvers/ADERDGSolver.cpp
@@ -2524,13 +2524,15 @@ void exahype::solvers::ADERDGSolver::restrictToTopMostParent( // TODO must be me
   assertion(parentCellDescription.getSolverNumber()==cellDescription.getSolverNumber());
   assertion1(cellDescription.getType()==CellDescription::Type::Descendant &&
              parentCellDescription.getHasToHoldDataForMasterWorkerCommunication(),cellDescription.toString());
+  #ifdef Parallel
+  assertion1(parentCellDescription.getType()==CellDescription::Type::Cell ||
+    (parentCellDescription.getType()==CellDescription::Type::Descendant &&
+    parentCellDescription.getHasToHoldDataForMasterWorkerCommunication())
+    ,parentCellDescription.toString());
+  #else
   assertion1(parentCellDescription.getType()==CellDescription::Type::Cell
-             #ifdef Parallel
-             ||
-             (parentCellDescription.getType()==CellDescription::Type::Descendant &&
-             parentCellDescription.getHasToHoldDataForMasterWorkerCommunication())
-             #endif
-             ,parentCellDescription.toString());
+    ,parentCellDescription.toString());
+  #endif
 
   std::vector<double>& updateFine   = DataHeap::getInstance().getData(cellDescription.getUpdate());
   std::vector<double>& updateCoarse = DataHeap::getInstance().getData(cellDescription.getUpdate());
