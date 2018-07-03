25f17a5637cc6258fcda2eb2b37cb505ff621912
Ben Hazelwood <benjamin.hazelwood@durham.ac.uk>
2018-07-03 11:54:23 +0100

    Turn off tree plot in mesh refinement

--- ExaHyPE/exahype/runners/Runner.cpp
+++ ExaHyPE/exahype/runners/Runner.cpp
@@ -751,7 +751,8 @@ bool exahype::runners::Runner::createMesh(exahype::repositories::Repository& rep
   bool meshUpdate = false;
 
   int meshSetupIterations = 0;
-  repository.switchToMeshRefinementAndPlotTree();
+  repository.switchToMeshRefinement();
+
 
   peano::parallel::loadbalancing::Oracle::getInstance().activateLoadBalancing(true);
   while (
