a572ec85027d780fd9352f584714d7f79aaf7879
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2018-08-21 22:58:45 +0200

    updateSubmodules: Comment out last two lines of jsonschema __init__.py.

--- Submodules/updateSubmodules.sh
+++ Submodules/updateSubmodules.sh
@@ -89,7 +89,7 @@ if [ $# -eq 0 ]; then
 		git pull origin master
 		cd ..
 	fi
-	#pyrsistent
+	#jsonschema
 	if [ ! -d jsonschema ]; then
 		mkdir jsonschema
 	fi
@@ -100,6 +100,9 @@ if [ $# -eq 0 ]; then
 		echo "Update jsonschema submodule"
 		cd jsonschema
 		git pull origin master
+                # comment last two lines of module init file (hot fix)
+                sed -i -e "s,^from pkg_resources import get_distribution,#from pkg_resources import get_distribution,g" jsonschema/__init__.py
+                sed -i -e "s,^__version__ = get_distribution(__name__).version,#__version__ = get_distribution(__name__).version,g" jsonschema/__init__.py
 		cd ..
 	fi
 	#Libxsmm
