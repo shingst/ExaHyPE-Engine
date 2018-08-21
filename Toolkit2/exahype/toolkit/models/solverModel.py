e66e5e3ec21ce5e1f1a71356920ea64f613c1019
jm <jean-matthieu.gallard@tum.de>
2018-08-21 17:54:02 +0200

    TODO Dominic - fix missing template

--- Toolkit2/exahype/toolkit/models/solverModel.py
+++ Toolkit2/exahype/toolkit/models/solverModel.py
@@ -61,8 +61,10 @@ class SolverModel(AbstractModelBaseClass):
         solverTemplates = {
             "user"    : [ (self.context["solver"]+".h"   , "solvers/MinimalFiniteVolumesSolverHeader.template"),
                           (self.context["solver"]+".cpp" , "solvers/EmptyFiniteVolumesSolverImplementation.template") ],
-            "generic" : [ (self.context["solver"]+".h"   , "solvers/FiniteVolumesHeader.template"),
-                          (self.context["solver"]+".cpp" , "solvers/FiniteVolumesInCUserCode.template") ],
+            "generic" : []
+            #TODO Dominic: don't exist
+            #[ (self.context["solver"]+".h"   , "solvers/FiniteVolumesHeader.template"),
+                        #  (self.context["solver"]+".cpp" , "solvers/FiniteVolumesInCUserCode.template") ],
         }
         
         abstractSolverTemplates  = { 
