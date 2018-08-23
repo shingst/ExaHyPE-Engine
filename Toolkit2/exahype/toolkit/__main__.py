662f7077be8f28efd4b835ab39163abf2e492236
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2018-08-23 16:19:42 +0200

    New toolkit requires max 24 MB memory per call on laptop.

--- Toolkit2/exahype/toolkit/__main__.py
+++ Toolkit2/exahype/toolkit/__main__.py
@@ -3,6 +3,8 @@
 import sys
 import os
 
+import resource
+
 def main():
     """"Call the controller and run it"""
     # append directory above to sys.path to be able to load the module
@@ -12,8 +14,16 @@ def main():
     control = Controller()
     control.run()
 
+def memoryUsageResource():
+    """
+    see: http://fa.bianp.net/blog/2013/different-ways-to-get-memory-consumption-or-lessons-learned-from-memory_profiler/
+    """
+    rusage_denom = 1024.
+    mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / rusage_denom
+    return mem
 
 if __name__ == "__main__":
     # execute only if run as a script
     main()
+    #print("Maximum memory usage [MiB]: %1.3f" % memoryUsageResource())
 
