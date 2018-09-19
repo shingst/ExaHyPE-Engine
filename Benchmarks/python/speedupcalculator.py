e5ee775caae29ef03857994aac4bf9436554d530
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2018-09-19 10:51:06 +0100

    Add tools for calculating speedups. i
    Result can be stripped and key columns enumerated such that it is suitable for any tikz plot.
    
    > ./tableslicer.py experiments/Euler_ADERDG/studies/0-base/results/Euler.csv --filter order=8 fused=true maximumMeshDepth=5 adapter=FusedTimeStep predictorBackground=false --cols cores consumerTasks total_realtime | ./speedupcalculator.py --enumerate-keys --no-header --no-data
    
    0,1.00
    1,1.00
    2,1.18
    3,2.06
    4,1.90
    5,2.11
    6,3.25
    7,3.45
    8,3.44
    9,5.92
    10,6.33
    11,6.23
    12,8.30
    13,8.22
    14,8.03
    15,10.88
    16,11.09
    17,10.94

---
+++ Benchmarks/python/speedupcalculator.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+def parseArgs():
+    parser = argparse.ArgumentParser(
+        description="This is speedupcalculatorpy: A small tool for computing speedups for data stored in a CSV table. The last column of a table row is assumed to be the data column. The first row is assumed to store reference value.",
+            epilog="End of help message.",
+    )
+    parser.add_argument('--header', dest='header', action='store_true',help="Write a header to the output file.")
+    parser.add_argument('--no-header', dest='header', action='store_false',help="Write no header to the output file.")
+    parser.set_defaults(header=True)
+    
+    parser.add_argument("--enumerate-keys", dest="enumerateKeys", action="store_true",help="Enumerate the key columns instead of including them.")
+    parser.add_argument("--no-enumerate-keys", dest="enumerateKeys", action="store_false",help="Include the key columns.")
+    parser.set_defaults(enumerateKeys=False)
+    
+    parser.add_argument("--data", dest="keepData", action="store_true",help="Include the original data column into the output file.")
+    parser.add_argument("--no-data", dest="keepData", action="store_false")
+    parser.set_defaults(keepData=True)
+    
+    parser.add_argument("table",
+       type=argparse.FileType("r"),nargs="?",
+       help="The CSV table to work on.",
+       default=sys.stdin)
+    
+    parser.add_argument("-o","--output",
+       type=argparse.FileType("w"),nargs="?",
+       help="Output file",
+       default=sys.stdout)
+    
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    import sys,os
+    import csv
+    import collections
+    import itertools
+    import argparse
+    
+    import math
+        
+    args = parseArgs()
+
+    # read table
+    firstRow  = next(args.table)
+    firstRow  = firstRow.strip().split(",")
+    tableData = list(csv.reader(args.table,delimiter=","))
+    args.table.close()
+
+    # check if has header; include header into output if requested 
+    result = []
+    try:
+        float(firstRow[-1])
+        tableData.insert(0,firstRow)
+    except:
+       if args.header:
+           if not args.enumerateKeys:
+               if not args.keepData:
+                  result.append(firstRow[:-1])
+               else:
+                  result.append(firstRow)
+               result[0].append("speedup")
+           else:
+               if not args.keepData:
+                   result.append(["n","speedup"])
+               else:
+                   result.append(["n",firstRow[-1],"speedup"])
+ 
+    
+    reference = float(tableData[0][-1])
+    n = 0
+    for row in tableData:
+        resultRow = None
+        if not args.enumerateKeys:
+            resultRow = row
+        else:
+            resultRow = [n,row[-1]]
+        if not args.keepData:
+            resultRow = resultRow[:-1]
+        resultRow.append("%1.2f" % ( reference/float(row[-1])) ) # compute speedup
+        result.append(resultRow)
+        n += 1
+    
+    csvwriter = csv.writer(args.output)
+    csvwriter.writerows(result)
