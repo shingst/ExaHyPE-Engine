68a366e17dcc5e270f1cd12edd8024085d387738
Lukas Krenz <lukas@krenz.land>
2018-09-26 12:29:56 +0200

    Fix Taylor-Green-vortex scenario for NS-Eq.

--- ApplicationExamples/CompressibleNavierStokes/Scenarios/TaylorGreen.cpp
+++ ApplicationExamples/CompressibleNavierStokes/Scenarios/TaylorGreen.cpp
@@ -1,62 +1,41 @@
 #include "TaylorGreen.h"
 
-void NavierStokes::TaylorGreen::initialValues(const double* const x,
-                                              const PDE& ns, Variables& vars) {
-  assert(DIMENSIONS == 2);
-  // 2D-Scenario
-  vars.rho() = 1.0;
-  vars.j(0) = 1 * std::cos(x[0]) * std::sin(x[1]);
-  vars.j(1) = -1 * std::sin(x[0]) * std::cos(x[1]);
-#if DIMENSIONS == 3
-  vars.j(2) = 0;
-#endif
-  const double pressure =
-      -1 * (vars.rho() / 4) * (std::cos(2 * x[0]) + std::cos(2 * x[1]));
-
-  vars.E() = ns.evaluateEnergy(vars.rho(), pressure, vars.j());
-}
 void NavierStokes::TaylorGreen::analyticalSolution(const double* const x,
                                                    const double t,
                                                    const PDE& ns,
                                                    Variables& vars,
                                                    double* gradState) {
+#if DIMENSIONS != 2
+  assertion2(false, "Taylor-Green is only supported for 2D!");
+#endif
+
   kernels::idx2 idxGradQ(DIMENSIONS, vars.SizeVariables);
 
   const double Ft = std::exp(-2 * ns.referenceViscosity * t);
 
   vars.rho() = 1.0;
-  vars.j(0) = 1 * std::cos(x[0]) * std::sin(x[1]) * Ft;
-  vars.j(1) = -1 * std::sin(x[0]) * std::cos(x[1]) * Ft;
-#if DIMENSIONS == 3
-  vars.j(2) = 0;
-#endif
-  const auto pressure = -1 * (vars.rho() / 4) *
-                        (std::cos(2 * x[0]) + std::cos(2 * x[1])) * Ft * Ft;
+  vars.j(0) =  1 * std::sin(x[0]) * std::cos(x[1]) * Ft;
+  vars.j(1) = -1 * std::cos(x[0]) * std::sin(x[1]) * Ft;
+
+  const double p_0 = 100./ns.gamma;
+  const double pressure =
+      p_0 + ((vars.rho() * Ft * Ft)/ 4) * (std::cos(2 * x[0]) + std::cos(2 * x[1]));
+
   vars.E() = ns.evaluateEnergy(vars.rho(), pressure, vars.j());
 
   // Assuming rho is constant.
   // j(0)
-  gradState[idxGradQ(0, 1)] = -1 * std::sin(x[0]) * std::sin(x[1]) * Ft;
-  gradState[idxGradQ(1, 1)] = 1 * std::cos(x[0]) * std::cos(x[1]) * Ft;
+  gradState[idxGradQ(0, 1)] =  1 * std::cos(x[0]) * std::cos(x[1]) * Ft;
+  gradState[idxGradQ(1, 1)] = -1 * std::sin(x[0]) * std::sin(x[1]) * Ft;
 
   // j(1)
-  gradState[idxGradQ(0, 2)] = -1 * std::cos(x[0]) * std::cos(x[1]) * Ft;
-  gradState[idxGradQ(1, 2)] = 1 * std::sin(x[0]) * std::sin(x[1]) * Ft;
+  gradState[idxGradQ(0, 2)] =  1 * std::sin(x[0]) * std::sin(x[1]) * Ft;
+  gradState[idxGradQ(1, 2)] = -1 * std::cos(x[0]) * std::cos(x[1]) * Ft;
 
-  // j(2) is zero for 3d and non-existant for 2d
+  // E
+  gradState[idxGradQ(0, 3)] = std::pow(Ft, 2)*((1.0/4.0)*(1.0*ns.gamma - 1.0)*(std::sin(2*x[0] - 2*x[1]) + std::sin(2*x[0] + 2*x[1])) + 0.5*std::sin(2*x[0]))/(ns.gamma - 1);
 
-  // E, idx 3 for 2d and idx 4 for 3d
-  // Assume that rho is 1 at boundary and constant (=zero derivative)
-  // TODO(Lukas) Fix these! They are wrong!
-  constexpr double e_idx = DIMENSIONS + 1;
-  const double e_factor = 0.25 * Ft * Ft;
-  gradState[idxGradQ(0, e_idx)] =
-      e_factor *
-      (1 * std::sin(2 * x[0] - 2 * x[1]) + std::sin(2 * x[0] + 2 * x[1]));
-  gradState[idxGradQ(1, e_idx)] =
-      e_factor *
-      (-1 * std::sin(2 * x[0] - 2 * x[1]) + std::sin(2 * x[0] + 2 * x[1]));
-  // gradState[idxGradQ(2, e_idx)] = 0.0;
+  gradState[idxGradQ(1, 3)] = std::pow(Ft, 2)*((1.0/4.0)*(1.0*ns.gamma - 1.0)*(-std::sin(2*x[0] - 2*x[1]) + std::sin(2*x[0] + 2*x[1])) + 0.5*std::sin(2*x[1]))/(ns.gamma - 1);
 }
 
 NavierStokes::BoundaryType NavierStokes::TaylorGreen::getBoundaryType(
--- ApplicationExamples/CompressibleNavierStokes/Scenarios/TaylorGreen.h
+++ ApplicationExamples/CompressibleNavierStokes/Scenarios/TaylorGreen.h
@@ -10,8 +10,6 @@
 namespace NavierStokes {
 
 class TaylorGreen : public Scenario {
-  void initialValues(const double* const x, const PDE& ns,
-                     Variables& vars) final override;
   void analyticalSolution(const double* const x, const double t, const PDE& ns,
                           Variables& vars, double* gradState) final override;
   BoundaryType getBoundaryType(int faceId) final override;
