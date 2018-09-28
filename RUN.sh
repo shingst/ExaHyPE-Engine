472dec71844873167936f0d3f61f862e361d33fd
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2018-09-28 09:51:22 +0100

    Update RUN.sh script.

--- RUN.sh
+++ RUN.sh
@@ -8,33 +8,27 @@
 # stop on error
 set -e
 
-# download the grid generation framework Peano.
-./Peano/updatePeano || ./Peano/fetchSnapshot.sh
-
-# compile the Java toolkit
-bash ./Toolkit/build.sh
-
-# Optional: Install libxsmm
-
-# git clone https://github.com/hfp/libxsmm.git Libxsmm
-# cd Libxsmm/
-# make generator
+# download the grid generation framework Peano
+# and a number of python dependencies
+# (use -h for information on different ways to clone the repositories.)
+bash Submodules/updateSubmodules.sh
 
 # Now you are ready to follow compile and run an ExaHyPE application
 # according to the guidebook:
-java -jar Toolkit/dist/ExaHyPE.jar --not-interactive ApplicationExamples/EulerFlow.exahype
+Toolkit/toolkit.sh Demonstrators/EulerFV/EulerFV.exahype
 
 # set build parameters
-export CC=GNU
-#export SHAREDMEM=TBB
+export COMPILER=Gnu
+export SHAREDMEM=None
+export DISTRIBUTEDMEM=None
 #export TBB_INC=/usr/include/tbb
 #export TBB_SHLIB="-L/usr/lib -ltbb"
 
-# build sample application
-cd ApplicationExamples/EulerFlow && make clean && make -j $(nproc)
+# build sample application (use -j for more make threads)
+( cd Demonstrators/EulerFV && make clean && make -j24 )
 
 # run sample application
-./ExaHyPE-EulerFlow ../EulerFlow.exahype
+( cd Demonstrators/EulerFV && ./ExaHyPE-EulerFV EulerFV.exahype )
 
 
 
