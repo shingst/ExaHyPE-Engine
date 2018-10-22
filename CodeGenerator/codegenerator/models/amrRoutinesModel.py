e2a130520e193df5ded257602ff87c240946c5f7
jm <jean-matthieu.gallard@tum.de>
2018-10-22 16:06:05 +0200

    CodeGen - bugfix AMR volume restriction, properly accumulate result + allocate/free only once

--- CodeGenerator/codegenerator/models/amrRoutinesModel.py
+++ CodeGenerator/codegenerator/models/amrRoutinesModel.py
@@ -52,7 +52,7 @@ class AMRRoutinesModel(AbstractModelBaseClass):
         nDofPad  = self.context["nDofPad"]
         nDim     = self.context["nDim"]
 
-        # Always overwrite input (no need to set to 0)
+        # Always overwrite input (no need to set to 0), except if add
         # nDim-1 face projection, inputs are padded
         self.context["matmulConfigs"]["face_Q_x"] =     MatmulConfig(nDataPad, nDof, nDof, nDataPad, nDofPad, nDataPad, 1, 0, 1, 1, "face_Q_x", "nopf", "gemm")
         self.context["matmulConfigs"]["face_F_x"] =     MatmulConfig(nVarPad , nDof, nDof, nVarPad , nDofPad, nVarPad , 1, 0, 1, 1, "face_F_x", "nopf", "gemm")
@@ -60,12 +60,14 @@ class AMRRoutinesModel(AbstractModelBaseClass):
             self.context["matmulConfigs"]["face_Q_y"] = MatmulConfig(nDataPad, nDof, nDof, nDataPad*nDof, nDofPad, nDataPad*nDof, 1, 0, 1, 1, "face_Q_y", "nopf", "gemm")
             self.context["matmulConfigs"]["face_F_y"] = MatmulConfig(nVarPad , nDof, nDof, nVarPad*nDof , nDofPad, nVarPad*nDof , 1, 0, 1, 1, "face_F_y", "nopf", "gemm")
         # nDim volume projection, luh (input/output) is not padded
-        self.context["matmulConfigs"]["volume_x"] =     MatmulConfig(nData   , nDof, nDof, nData         , nDofPad, nDataPad     , 1, 0, 0, 1, "volume_x", "nopf", "gemm") # input slice not aligned
+        self.context["matmulConfigs"]["volume_x"] =      MatmulConfig(nData   , nDof, nDof, nData         , nDofPad, nDataPad     , 1, 0, 0, 1, "volume_x", "nopf", "gemm") # input slice not aligned
         if(nDim==3):
-            self.context["matmulConfigs"]["volume_y"] = MatmulConfig(nDataPad, nDof, nDof, nDataPad*nDof , nDofPad, nDataPad*nDof, 1, 0, 1, 1, "volume_y", "nopf", "gemm")
-            self.context["matmulConfigs"]["volume_z"] = MatmulConfig(nData   , nDof, nDof, nDataPad*nDof2, nDofPad, nData*nDof2  , 1, 0, 1, 0, "volume_z", "nopf", "gemm") 
+            self.context["matmulConfigs"]["volume_y"] =  MatmulConfig(nDataPad, nDof, nDof, nDataPad*nDof , nDofPad, nDataPad*nDof, 1, 0, 1, 1, "volume_y", "nopf", "gemm")
+            self.context["matmulConfigs"]["volume_z"] =  MatmulConfig(nData   , nDof, nDof, nDataPad*nDof2, nDofPad, nData*nDof2  , 1, 0, 1, 0, "volume_z", "nopf", "gemm") # output slice not aligned
+            self.context["matmulConfigs"]["volume_z_add"] = MatmulConfig(nData, nDof, nDof, nDataPad*nDof2, nDofPad, nData*nDof2  , 1, 1, 1, 0, "volume_z_add", "nopf", "gemm") # output slice not aligned, add to result
         else:
-            self.context["matmulConfigs"]["volume_y"] = MatmulConfig(nData   , nDof, nDof, nDataPad*nDof , nDofPad, nData*nDof   , 1, 0, 1, 0, "volume_y", "nopf", "gemm") # output slice not aligned
+            self.context["matmulConfigs"]["volume_y"] =  MatmulConfig(nData   , nDof, nDof, nDataPad*nDof , nDofPad, nData*nDof   , 1, 0, 1, 0, "volume_y", "nopf", "gemm") # output slice not aligned
+            self.context["matmulConfigs"]["volume_y_add"] = MatmulConfig(nData, nDof, nDof, nDataPad*nDof , nDofPad, nData*nDof   , 1, 1, 1, 0, "volume_y_add", "nopf", "gemm") # output slice not aligned, add to result
         
         # TODO JMG Old gemms, to delete
         #-----------------------------
