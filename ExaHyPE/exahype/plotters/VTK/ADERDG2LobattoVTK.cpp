204e580254cfda9992a7ef64fb965c89ebeda345
Kenneth Duru <kduru@eiger.geophysik.uni-muenchen.de>
2018-11-02 15:54:22 +0100

    Adjusted to new dataHeap

--- ExaHyPE/exahype/plotters/VTK/ADERDG2LobattoVTK.cpp
+++ ExaHyPE/exahype/plotters/VTK/ADERDG2LobattoVTK.cpp
@@ -494,7 +494,7 @@ void exahype::plotters::ADERDG2LobattoVTK::plotPatch(const int cellDescriptionsI
   auto& aderdgCellDescription = exahype::solvers::ADERDGSolver::getCellDescription(cellDescriptionsIndex,element);
 
   if (aderdgCellDescription.getType()==exahype::solvers::ADERDGSolver::CellDescription::Type::Cell) {
-    double* solverSolution = DataHeap::getInstance().getData(aderdgCellDescription.getSolution()).data();
+    double* solverSolution = static_cast<double*>(aderdgCellDescription.getSolution());
 
     plotPatch(
         aderdgCellDescription.getOffset(),
