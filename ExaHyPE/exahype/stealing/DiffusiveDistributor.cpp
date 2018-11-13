910d6e85a72837761e2efafc7afdbd6fd1f652ae
Philipp Samfaß <samfass2@illinois.edu>
2018-11-13 18:35:32 +0100

    added missing files

---
+++ ExaHyPE/exahype/stealing/DiffusiveDistributor.cpp
@@ -0,0 +1,109 @@
+#if  defined(SharedTBB)  && defined(Parallel)
+#include "DiffusiveDistributor.h"
+
+#include <algorithm>
+#include <numeric>
+
+#include "tarch/multicore/Lock.h"
+#include "tarch/parallel/Node.h"
+
+#include "exahype/stealing/StealingProfiler.h"
+#include "exahype/stealing/PerformanceMonitor.h"
+#include "tarch/multicore/Core.h"
+#include "tarch/multicore/Jobs.h"
+
+tarch::logging::Log exahype::stealing::DiffusiveDistributor::_log( "exahype::stealing::DiffusiveDistributor" );
+
+exahype::stealing::DiffusiveDistributor::DiffusiveDistributor() :
+  _iterationTimer("DiffusiveDistributor","IterationTimer",false),
+  _isVictim(false)  
+{
+  int nnodes = tarch::parallel::Node::getInstance().getNumberOfNodes();
+
+  _tasksToOffload          = new int[nnodes];
+  _remainingTasksToOffload = new std::atomic<int>[nnodes];
+
+  std::fill( &_remainingTasksToOffload[0], &_remainingTasksToOffload[nnodes], 0);
+  std::fill( &_tasksToOffload[0], &_tasksToOffload[nnodes], 0);
+}
+
+exahype::stealing::DiffusiveDistributor::~DiffusiveDistributor() {
+  delete[] _tasksToOffload;
+  delete[] _remainingTasksToOffload;
+}
+
+void exahype::stealing::DiffusiveDistributor::updateLoadDistribution(int currentLoad) {
+
+  int nnodes = tarch::parallel::Node::getInstance().getNumberOfNodes();
+  int myRank = tarch::parallel::Node::getInstance().getRank();
+
+  int *loadSnapshot = new int[nnodes];
+  const int* currentLoadSnapshot = exahype::stealing::PerformanceMonitor::getInstance().getCurrentLoadSnapshot();
+  std::copy(&currentLoadSnapshot[0], &currentLoadSnapshot[nnodes], loadSnapshot);
+
+  loadSnapshot[myRank] = currentLoad;
+
+  logInfo("updateLoadDistribution()", "current load"<<currentLoad);
+
+  //determine who is fastest
+  int fastestRank = std::distance(&loadSnapshot[0], std::max_element(&loadSnapshot[0], &loadSnapshot[myRank]));
+
+  //determine who is slowest
+  int slowestRank = std::distance(&loadSnapshot[0], std::min_element(&loadSnapshot[0], &loadSnapshot[myRank]));
+  
+  logInfo("updateLoadDistribution()", "fastest: "<<fastestRank<<" slowest:"<<slowestRank);
+
+  if(myRank == slowestRank && !_isVictim) {
+    _tasksToOffload[fastestRank]++;
+    logInfo("updateLoadDistribution()", "increment, send "<<_tasksToOffload[fastestRank]<<" to rank "<<fastestRank );
+  }
+  else if(_tasksToOffload[slowestRank]>0) {
+    _tasksToOffload[slowestRank]--;
+    logInfo("updateLoadDistribution()", "decrement, send "<<_tasksToOffload[slowestRank]<<" to rank "<<slowestRank );
+  }
+
+  delete[] loadSnapshot;
+
+}
+
+exahype::stealing::DiffusiveDistributor& exahype::stealing::DiffusiveDistributor::getInstance() {
+  static DiffusiveDistributor diffusiveDist;
+  return diffusiveDist;
+}
+
+void exahype::stealing::DiffusiveDistributor::triggerVictimFlag() {
+  _isVictim = true;
+}
+
+void exahype::stealing::DiffusiveDistributor::resetVictimFlag() {
+  _isVictim = false;
+}
+
+bool exahype::stealing::DiffusiveDistributor::selectVictimRank(int& victim) {
+
+  int nnodes = tarch::parallel::Node::getInstance().getNumberOfNodes();
+  int myRank = tarch::parallel::Node::getInstance().getRank();
+  victim = myRank;
+
+  static std::atomic<int> rank_cnt = 0;
+
+  int l_rank = rank_cnt;
+
+  for(int i=0; i<nnodes; i++) {
+    if(l_rank!=myRank && _remainingTasksToOffload[l_rank].fetch_sub(1)>0) {
+      victim = l_rank;
+      l_rank = (l_rank + 1)%nnodes;
+      break;
+    }
+    else
+    _remainingTasksToOffload[l_rank]=0;
+    l_rank = (l_rank + 1)%nnodes;
+  }
+  rank_cnt=l_rank;
+
+  if(victim!=myRank)
+   logInfo("selectVictimRank", "chose victim "<<victim<<" _remainingTasksToOffload "<<_remainingTasksToOffload[victim]);
+  
+  return victim != myRank;
+}
+#endif
---
+++ ExaHyPE/exahype/stealing/DiffusiveDistributor.h
@@ -0,0 +1,45 @@
+#if !defined(_EXAHYPE_STEALING_DIFFUSIVEDISTRIBUTOR_H_) && defined(SharedTBB)  && defined(Parallel)
+#define _EXAHYPE_STEALING_DIFFUSIVEDISTRIBUTOR_H_
+
+#include "tarch/logging/Log.h"
+#include "tarch/multicore/BooleanSemaphore.h"
+#include "tarch/timing/Watch.h"
+
+#include <mpi.h>
+#include <atomic>
+
+namespace exahype {
+  namespace stealing {
+    class DiffusiveDistributor;
+  }
+}
+
+class exahype::stealing::DiffusiveDistributor {
+  private:
+    static tarch::logging::Log _log;
+    DiffusiveDistributor();
+
+    // stores how many tasks should be offloaded in every time step
+    int *_tasksToOffload;
+    // stores how many tasks still need to be offloaded in the current time step
+    std::atomic<int> *_remainingTasksToOffload;
+    
+    std::atomic<bool> _isVictim;
+
+  public:
+    tarch::timing::Watch _iterationTimer;
+
+    static DiffusiveDistributor& getInstance();
+    virtual ~DiffusiveDistributor();
+
+    void updateLoadDistribution(int load);
+
+    // return next victim rank
+    bool selectVictimRank(int& victim);
+
+    void triggerVictimFlag();
+    void resetVictimFlag();
+
+};
+
+#endif
