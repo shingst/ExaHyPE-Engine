2517ab3717f32cd533d82da202844bec95e6ee06
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2018-11-14 11:08:41 +0000

    Consider API change in ADERDGSolver source.

--- ExaHyPE/exahype/solvers/ADERDGSolver.cpp
+++ ExaHyPE/exahype/solvers/ADERDGSolver.cpp
@@ -3237,6 +3237,7 @@ void exahype::solvers::ADERDGSolver::solveRiemannProblemAtInterface(
 
   riemannSolver(
       FL,FR,QL,QR,
+      std::min( pLeft.getCorrectorTimeStamp(),pRight.getCorrectorTimeStamp() ),
       std::min( pLeft.getCorrectorTimeStepSize(),pRight.getCorrectorTimeStepSize() ),
       face._direction, false, -1); // TODO(Dominic): Merge Riemann solver directly with the face integral and push the result on update
                                    // does not make sense to overwrite the flux when performing local time stepping; coarse grid flux must be constant, or not?
@@ -4081,6 +4082,7 @@ void exahype::solvers::ADERDGSolver::solveRiemannProblemAtInterface(
 
     riemannSolver(
         FL, FR, QL, QR,
+        cellDescription.getCorrectorTimeStamp(),
         cellDescription.getCorrectorTimeStepSize(),face._direction,false,face._faceIndex);
     
     #ifdef Asserts
@@ -4104,6 +4106,7 @@ void exahype::solvers::ADERDGSolver::solveRiemannProblemAtInterface(
 
     riemannSolver(
         FL, FR, QL, QR,
+        cellDescription.CorrectorTimeStamp()
         cellDescription.getCorrectorTimeStepSize(),face._direction,false,face._faceIndex);
     
     #ifdef Asserts
