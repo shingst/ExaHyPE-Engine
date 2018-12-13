7b5936e04d5c7cd986d121c374c60b2971024157
Lukas Krenz <lukas@krenz.land>
2018-12-13 21:57:08 +0100

    Fix detonation scenario.

--- ApplicationExamples/CompressibleNavierStokes/Scenarios/Detonation.cpp
+++ ApplicationExamples/CompressibleNavierStokes/Scenarios/Detonation.cpp
@@ -3,6 +3,11 @@
 void NavierStokes::Detonation::initialValues(const double* const x,
                                              const PDE& ns, Variables& vars) {
 
+  assert(ns.useAdvection);
+
+  const auto centerX = 0.0;
+  const auto centerY = 0.0;
+
   // B: Burned, U: Unburned
   const auto rhoB = 1.4;
   const auto rhoU = 0.887565;
@@ -11,16 +16,14 @@ void NavierStokes::Detonation::initialValues(const double* const x,
   const auto ZB = 0.0;
   const auto ZU = 1.0;
 
-  const auto alpha = std::atan2(x[1], x[0]); // Angle in polar coordinates
+  const auto alpha = std::atan2(x[1] - centerX, x[0] - centerY); // Angle in polar coordinates
   const auto uB = 0.0;
   const auto uU = -0.577350 * std::cos(alpha);
   const auto vB = 0.0;
   const auto vU = -0.577350 * std::sin(alpha);
 
-  const auto centerX = 0.5;
-  const auto centerY = 0.5;
-  const auto distX = x[0] - centerX;
-  const auto distY = x[1] - centerY;
+  const auto distX = x[0];
+  const auto distY = x[1];
   const auto distToCenter = std::sqrt(
           distX * distX + distY * distY
           );
@@ -36,7 +39,7 @@ void NavierStokes::Detonation::initialValues(const double* const x,
 #else
     vars.j(uB * rhoB, vB * rhoB, 0.0);
 #endif
-     ns.setZ(vars.data(), ZB);
+     ns.setZ(vars.data(), ZB * rhoB);
   } else {
      vars.rho() = rhoU;
      pressure = pU;
@@ -45,12 +48,13 @@ void NavierStokes::Detonation::initialValues(const double* const x,
 #else
      vars.j(uU * rhoU, vU * rhoU, 0.0);
 #endif
-     ns.setZ(vars.data(), ZU);
+     ns.setZ(vars.data(), ZU * rhoU);
   }
 
   vars.E() = ns.evaluateEnergy(vars.rho(), pressure, vars.j(), ns.getZ(vars.data()));
 
   assert(ns.getZ(vars.data()) >= 0 && ns.getZ(vars.data()) <= 1.0);
+  assert(ns.getZ(vars.data()) == 0 || ns.getZ(vars.data()) == rhoU);
 }
 double NavierStokes::Detonation::getMolecularDiffusionCoeff() const {
   return 0.0; // Euler, for now.
@@ -65,17 +69,33 @@ void NavierStokes::Detonation::source(
   Scenario::source(x, t, ns, Q, S);
   const auto vars = ReadOnlyVariables{Q};
 
-  const auto pressure = ns.evaluatePressure(vars.E(), vars.rho(), vars.j(), Q[vars.size()]);
+  const auto pressure = ns.evaluatePressure(vars.E(), vars.rho(), vars.j(), ns.getZ(Q));
   const auto T = ns.evaluateTemperature(vars.rho(), pressure);
 
   const auto reactionTimeScale = 0.1; // TODO(Lukas) Try stiff ts?
   const auto ignitionT = 0.26;
+  //const auto ignitionT = 0.4;
   if (T > ignitionT) {
     // Critical temperature, start decay.
     ns.setZ(S, -1.0/reactionTimeScale * ns.getZ(Q));
   } else {
     ns.setZ(S, 0.0);
   }
+}
+
+const double NavierStokes::Detonation::getGamma() const { return 1.4; }
+
+const double NavierStokes::Detonation::getPr() const {
+  // TODO(Lukas) What's the best value?
+  return 0.7;
+}
+
+const double NavierStokes::Detonation::getC_v() const { return 2.5; }
+
+const double NavierStokes::Detonation::getC_p() const { return 3.5; }
+
+const double NavierStokes::Detonation::getGasConstant() const {
+  return 1.0;
 }
 
 bool NavierStokes::Detonation::getUseAdvection() const {
