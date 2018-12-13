a93ad24f0fd180b86c4142eeb61754ef0c7dac0a
ffambri <francesco.fambri@unitn.it>
2018-12-13 17:28:52 +0100

    Added auxiliary Application "GRMHDb". This is a good interface between the "Ferrari-90" code, at the moment only for the GRMHD equations.
    Michel accretion has been tested successfully in serial,
    MPI does not work with the version of the 5 December 2018: further tests are needed.
    
    The main file is GRMHD_mod.f90, it contains all the PDE.
    
    I didn't commit the libtecio.a file, we should put it somewhere in the Miscellaneous folder.
    (If needed, take it e.g. from .\ApplicationExamples\DIM\DIM_3DLADGOM\lib folder)

---
+++ ApplicationExamples/GRMHDb/GRMHDb/NSTOV_modVar.f90
@@ -0,0 +1,13 @@
+   
+ MODULE NSTOV_modVar
+    IMPLICIT NONE 
+#if defined(RNSTOV) 
+    REAL, DIMENSION (:), POINTER :: t,void1,void2,void3
+    REAL, DIMENSION (:,:), POINTER :: y
+    REAL, DIMENSION (:,:,:), POINTER :: yDG
+    !
+#endif     
+ END MODULE NSTOV_modVar
+    
+        
+     
---
+++ ApplicationExamples/GRMHDb/GRMHDb/TECPLOTinterface.f90
@@ -0,0 +1,28 @@
+RECURSIVE SUBROUTINE ElementCallTECPLOTPLOTTER(wh,lx0,ldx,limiter)
+	USE TECPLOTPLOTTERmod
+	USE Parameters, only : nVar, nDim
+	implicit none
+	REAL, INTENT(IN) :: wh(nVar,nDOFm),lx0(nDim),ldx(nDim)
+	real :: lx0_3(3),ldx_3(3)
+	integer :: limiter
+	lx0_3(1:nDim)=lx0
+	ldx_3(1:nDim)=ldx
+	CALL ElementTECPLOTPLOTTER(wh,lx0_3,ldx_3,limiter)
+END SUBROUTINE ElementCallTECPLOTPLOTTER
+
+RECURSIVE SUBROUTINE InitializeTECPLOTPLOTTER(time)
+	USE TECPLOTPLOTTERmod
+	implicit none
+	real :: time
+	CALL InitTECPLOTPLOTTER(time)
+END SUBROUTINE InitializeTECPLOTPLOTTER
+
+RECURSIVE SUBROUTINE FinishTECPLOTPLOTTER(Myrank)
+	USE TECPLOTPLOTTERmod
+	implicit none
+	integer :: Myrank
+	CALL FinalizeTECPLOTPLOTTER(Myrank)
+   ! PRINT *, "FINALIZETECPLOTPLOTTER : done!"
+END SUBROUTINE FinishTECPLOTPLOTTER
+
+
