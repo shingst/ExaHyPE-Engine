39757a6ad268dc139a1f252753f52b50d25f3e5b
Lukas Krenz <lukas@krenz.land>
2018-12-14 22:15:34 +0100

    Add AMRSettings.

---
+++ ApplicationExamples/CompressibleNavierStokes/AMR/AMRSettings.cpp
@@ -0,0 +1,14 @@
+#include "AMRSettings.h"
+
+NavierStokes::IndicatorVariable NavierStokes::parseIndicatorVariable(
+    const std::string& name) {
+  if (name == "rho") {
+    return IndicatorVariable::rho;
+  } else if (name == "pressure") {
+    return IndicatorVariable::pressure;
+  } else if (name == "potential-temperature") {
+    return IndicatorVariable::potentialTemperature;
+  } else {
+    throw -1;
+  }
+}
