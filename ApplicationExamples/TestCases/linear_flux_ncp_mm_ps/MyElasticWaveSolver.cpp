ca5a247d69b44acf8048beefea7710ded8d696f0
jm <jean-matthieu.gallard@tum.de>
2019-01-31 13:21:52 +0100

    fix testcase linear_flux_ncp_mm_ps

--- ApplicationExamples/TestCases/linear_flux_ncp_mm_ps/MyElasticWaveSolver.cpp
+++ ApplicationExamples/TestCases/linear_flux_ncp_mm_ps/MyElasticWaveSolver.cpp
@@ -521,7 +521,7 @@ void Elastic::MyElasticWaveSolver::multiplyMaterialParameterMatrix_vect(const do
   
 }
 
-void Elastic::MyElasticWaveSolver::riemannSolver(double* FL_,double* FR_,const double* const QL_,const double* const QR_,const double dt,const int normalNonZeroIndex, bool isBoundaryFace, int faceIndex){
+void Elastic::MyElasticWaveSolver::riemannSolver(double* FL_,double* FR_,const double* const QL_,const double* const QR_,const double t,const double dt,const int normalNonZeroIndex, bool isBoundaryFace, int faceIndex){
 
 #ifdef OPT_KERNELS
   double FL[converter::getFFaceGenArraySize()];
