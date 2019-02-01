8a9fab825f77f5c87df6689ca330bed9886603d5
Francesco Fambri <di68hom3@login23.sm.lrz.de>
2019-02-01 01:02:29 +0100

    GRMHDb_ADERDG_FV: ErrorWriter and auxiliary functions.

--- ApplicationExamples/GRMHDb/GRMHDb_ADERDG_FV/Tools.h
+++ ApplicationExamples/GRMHDb/GRMHDb_ADERDG_FV/Tools.h
@@ -2,8 +2,9 @@
 // Fortran functions:
 extern "C" {
 //
+void pdecons2primfix_(double* uPrim, const double* uCons,int* iErr);
 void getnumericalsolution_(double* V,double* Q);
-void getexactsolution_(double* V,double* pos,double* timeStamp);
+void getexactsolution_(double* x,double* timestep,double* V);
 void inittecplot_(const int* N_in,const int* M_in, const int* basisSize, const int* Ghostlayers);
 //
 }/* extern "C" */
