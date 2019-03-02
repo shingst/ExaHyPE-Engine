027205deb6f3edbe4a7b045dfabc0c272ea906ff
Sven K <sven@nils.local>
2019-03-02 23:28:13 +0100

    Applications/GRMHD: Updating for new API.
    Apparently some functions gone.

--- ApplicationExamples/GRMHD/GRMHD/GRMHDSolver_ADERDG.h
+++ ApplicationExamples/GRMHD/GRMHD/GRMHDSolver_ADERDG.h
@@ -159,11 +159,6 @@ class GRMHD::GRMHDSolver_ADERDG : public GRMHD::AbstractGRMHDSolver_ADERDG {
       const tarch::la::Vector<DIMENSIONS,double>& dx,
       const double t) const override;
 
-   void mapDiscreteMaximumPrincipleObservables(
-       double* const observables,const int NumberOfVariables,
-       const double* const Q) const override;
-
-
 
 /* multiplyMaterialParameterMatrix() not included, as requested in the specification file */
 };
