653858869e1063202d7acd1923fbebe2bfb4ad21
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2019-03-05 22:07:12 +0000

    MeshRefinement: Reintroduce stable iteration counter. Untested. Might not compile. Wait 5 min.

--- ExaHyPE/exahype/mappings/MeshRefinement.h
+++ ExaHyPE/exahype/mappings/MeshRefinement.h
@@ -70,6 +70,16 @@ private:
    * for all solver.
    */
   bool _allSolversAttainedStableState = false;
+
+  /**
+   * The number of iterations in a row where
+   * all solvers attained a stable state.
+   * This counter is used for delaying erasing
+   * iterations till all solvers
+   * have finished their flagging.
+   */
+  int _stableIterationsInARow = 0;
+
   /**
    * A state indicating if vertical (master-worker) exchange
    * of face data is required during the time stepping iterations
