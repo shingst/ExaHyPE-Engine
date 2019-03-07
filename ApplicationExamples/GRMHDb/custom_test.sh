7c80e7d4e72f24732a3754d1a7af781881077411
Francesco Fambri <francesco.fambri@unitn.it>
2019-03-07 15:36:03 +0100

    GRMHDb: some uptated files.

--- ApplicationExamples/GRMHDb/custom_test.sh
+++ ApplicationExamples/GRMHDb/custom_test.sh
@@ -3,9 +3,9 @@
 export COMPILER=Intel
 export MODE=Release
 export SHAREDMEM=TBB
-export DISTRIBUTEDMEM=MPI
-export OMP_NUM_THREADS=10              # numero di threads per processo
-export USE_IPO=off
+export DISTRIBUTEDMEM=None
+export OMP_NUM_THREADS=28              # numero di threads per processo
+#export USE_IPO=off
 # this is for IBM MPI
 #export MP_TASK_AFFINITY=core:$OMP_NUM_THREADS   # numero di cores assegnati ai threads
 #if [ "$OMP_NUM_THREADS" -gt 1 ] ; then
