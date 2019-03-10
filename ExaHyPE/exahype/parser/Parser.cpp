2c7f8f585edb54630618838f75c81534535eaca8
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2019-03-10 22:34:11 +0000

    Add option to choose mesh spacing such that 1/3+2 of cells of bounding box are automatically placed outside.
    This way the coarsest populated mesh is distributed among 2^d instead of 3^d workers (assuming cubic domain).
    This allows using 2^d, 2^d * 3^d, ..., 2^d * 3^(i*d) ranks.
    
    I think it now makes sense to have a python tool which tells the user a
    priori what mesh will be created given their selected options.
    Integrate it into toolkit?

--- ExaHyPE/exahype/parser/Parser.cpp
+++ ExaHyPE/exahype/parser/Parser.cpp
@@ -527,6 +527,14 @@ int exahype::parser::Parser::getOutsideCellsLeft() const {
   return result;
 }
 
+bool exahype::parser::Parser::getPlaceOneThirdOfCellsOuside() const {
+  return getBoolFromPath("/computational_domain/one_third_of_cells_outside", false, isOptional);
+}
+
+bool exahype::parser::Parser::getScaleBoundingBox() const {
+  return getPlaceOneThirdOfCellsOuside() || getOutsideCells() > 0;
+}
+
 std::string exahype::parser::Parser::getMulticorePropertiesFile() const {
   std::string result = getStringFromPath("/shared_memory/properties_file","shared-memory.properties",isOptional);
   logDebug("getMulticorePropertiesFile()", "found " << result);
