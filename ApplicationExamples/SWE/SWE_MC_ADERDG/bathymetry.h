fbf0bcd5d69eff307af3a377c669fea820d83af5
Anne Reinarz <reinarz@in.tum.de>
2019-03-25 17:35:30 +0100

    ADERDG version

---
+++ ApplicationExamples/SWE/SWE_MC_ADERDG/bathymetry.h
@@ -0,0 +1,12 @@
+#ifndef __Bathymetry_CLASS_HEADER__
+#define __Bathymetry_CLASS_HEADER__
+
+#include <random>
+
+namespace SWE {
+
+    double bathymetry(double x, double y);
+    double linearInterpolation(double x, double y);
+}
+
+#endif
