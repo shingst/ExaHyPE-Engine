e6db3f87b8a42590599d3fa909df2e4f0015a79a
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2019-03-26 18:04:20 +0000

    Add script that clones the latest version of the Submodules. Can be copied to a place where it can be accessed via the logins of SuperMUC.

---
+++ Miscellaneous/Submodules/download-submodules.sh
@@ -0,0 +1,9 @@
+#!usr/bin/env bash
+git clone https://gitlab.lrz.de/gi26det/Peano.git
+git clone https://github.com/pallets/jinja.git
+git clone https://github.com/pallets/markupsafe.git
+git clone https://github.com/python-attrs/attrs.git
+git clone https://github.com/tobgu/pyrsistent.git
+git clone https://github.com/Julian/jsonschema.git
+git clone https://github.com/benjaminp/six.git
+git clone https://github.com/hfp/libxsmm.git 
