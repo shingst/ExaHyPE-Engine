cfd6fcef2900192cf6cc3b83fc00b1759614721b
Leonhard Rannabauer <leo.rannabauer@tum.de>
2019-03-29 17:51:47 +0100

    Added support for toolkit 2

--- Miscellaneous/Jenkins/Matrix.groovy
+++ Miscellaneous/Jenkins/Matrix.groovy
@@ -4,7 +4,7 @@ import Config
 
 def getMatrix() {
     // Warning: This might break down if more than one exahpye file is in a directory!
-    def files = findFiles(glob: 'ApplicationExamples/TestCases/*/*.exahype')
+    def files = findFiles(glob: 'ApplicationExamples/TestCases/*/*.exahype*')
     files.each { d -> echo d.getPath() }
     testCases = []
     for (file in files) {
