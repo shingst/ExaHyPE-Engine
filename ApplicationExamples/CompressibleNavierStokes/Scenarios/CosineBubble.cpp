8a720b4f460bf7a42c9ad565c3b99b5d180a5731
Lukas Krenz <lukas@krenz.land>
2019-05-02 15:11:52 +0200

    Fix parameter of 3d cosine bubble.

--- ApplicationExamples/CompressibleNavierStokes/Scenarios/CosineBubble.cpp
+++ ApplicationExamples/CompressibleNavierStokes/Scenarios/CosineBubble.cpp
@@ -1,7 +1,7 @@
 #include "CosineBubble.h"
 
 NavierStokes::CosineBubble::CosineBubble() {
-  const auto tempDiff = DIMENSIONS == 2 ? 0.5 : 1.0;
+  const auto tempDiff = DIMENSIONS == 1.0;
   const auto x = 500;     // [m]
   const auto z = DIMENSIONS == 2 ? 350 : 260;     // [m]
   const auto size = 250;  // [m]
