cf9e3df72a98e729b44d9d9046270bd12ab9f94a
Dominic Etienne Charrier <dominic.charrier@googlemail.com>
2019-05-29 21:30:04 +0100

    Checkout accidentally overwritten older versions of Euler/Euler solver files.

--- ApplicationExamples/Euler/Euler/EulerSolver_ADERDG.h
+++ ApplicationExamples/Euler/Euler/EulerSolver_ADERDG.h
@@ -220,15 +220,19 @@ public:
       const tarch::la::Vector<DIMENSIONS,double>& dx,
       const double t) const override;
   
- bool vetoDiscreteMaximumPrincipleDecision(
-      const double* const                         solution,
-      const double* const                         localDMPObservablesMin,
-      const double* const                         localDMPObservablesMax,
-      const bool                                  wasTroubledInPreviousTimeStep,
-      const tarch::la::Vector<DIMENSIONS,double>& cellCentre,
-      const tarch::la::Vector<DIMENSIONS,double>& cellSize,
-      const double                                timeStamp) const override { 
-        return !tarch::la::equals(0,timeStamp); /* do not veto DMP*/ 
-      } 
+  void resetGlobalObservables(GlobalObservables& globalObservables) const final override;
+
+  void mapGlobalObservables(
+      GlobalObservables&                          globalObservables,
+      const double* const                         luh,
+      const tarch::la::Vector<DIMENSIONS,double>& cellSize) const final override;
+
+  void mergeGlobalObservables(
+      GlobalObservables&         globalObservables,
+      ReadOnlyGlobalObservables& otherObservables) const final override;
+  
+  void beginTimeStep(const double minTimeStamp,const bool isFirstTimeStepOfBatchOrNoBatch) final override;
+
+  };
 
 #endif // __EulerSolver_ADERDG_CLASS_HEADER__
