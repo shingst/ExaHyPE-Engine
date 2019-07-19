2cba4d3a0396028c9a455a0a0672a508dd92c611
Philipp Samfass <ps659535@login-t.hpc.itc.rwth-aachen.de>
2019-07-19 11:15:09 +0200

    fixed issues with limiting aderdg solver

--- ExaHyPE/exahype/solvers/LimitingADERDGSolver.h
+++ ExaHyPE/exahype/solvers/LimitingADERDGSolver.h
@@ -613,7 +613,7 @@ private:
         const bool            isLastTimeStepOfBatch,
         const bool            isSkeletonJob);
 
-    bool run() override;
+    bool run(bool runOnMasterThread) override;
   };
 
   /**
@@ -655,7 +655,7 @@ private:
         CellInfo&             cellInfo,
         const bool            isAtRemoteBoundary);
 
-      bool run() override;
+      bool run(bool runOnMasterThread) override;
       void prefetchData() override;
   };
 
@@ -700,7 +700,7 @@ private:
         const tarch::la::Vector<DIMENSIONS_TIMES_TWO,signed char>& limiterNeighbourMergePerformed,
         const bool                                                 isAtRemoteBoundary);
 
-    bool run() override;
+    bool run(bool runOnMasterThread) override;
     void prefetchData() override;
   };
 
@@ -720,7 +720,7 @@ private:
         CellInfo&             cellInfo,
         const bool            isInitialMeshRefinement);
 
-    bool run() override;
+    bool run(bool runOnMasterThread) override;
   };
 
 public:
@@ -1615,7 +1615,10 @@ void updateNextGlobalObservables(
   void updateGlobalObservables(
       double* const                               globalObservables,
       const double* const                         luh,
-      const tarch::la::Vector<DIMENSIONS,double>& cellSize) final override;
+      const tarch::la::Vector<DIMENSIONS,double>& cellCentre,
+      const tarch::la::Vector<DIMENSIONS,double>& cellSize,
+      const double t,
+      const double dt) final override;
 
   void mergeGlobalObservables(
       double* const       globalObservables,
