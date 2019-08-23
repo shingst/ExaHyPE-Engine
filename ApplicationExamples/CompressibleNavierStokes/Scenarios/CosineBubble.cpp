bdd0f3b3d7fe872c76a4ec3ac4189e9d14105139
Lukas Krenz <lukas.krenz@in.tum.de>
2019-08-23 11:11:29 +0200

    Fix NS cos bubble for 3D

--- ApplicationExamples/CompressibleNavierStokes/Scenarios/CosineBubble.cpp
+++ ApplicationExamples/CompressibleNavierStokes/Scenarios/CosineBubble.cpp
@@ -3,7 +3,7 @@
 NavierStokes::CosineBubble::CosineBubble() {
   const auto tempDiff = DIMENSIONS == 1.0;
   const auto x = 500;     // [m]
-  const auto z = DIMENSIONS == 2 ? 350 : 260;     // [m]
+  const auto z = 350;     // [m]
   const auto size = 250;  // [m]
   const auto decay = -1;  // doesn't matter for cosine bubble
 
