5ddc38d23504e8e677b4c1463b8d9b422f6b6eed
Philipp Samfaß samfass@in.tum.de (4915739314827) <samfass@login01.hpcadvisorycouncil.com>
2020-12-27 04:23:57 -0800

    changed filenames in extract plot data

--- Miscellaneous/teaMPITaskSharing/extract-plot-data.sh
+++ Miscellaneous/teaMPITaskSharing/extract-plot-data.sh
@@ -1,5 +1,8 @@
-inputFiles=("./Output_baseline_ucx_patched_lto/results/Elastic-timestep-times.csv" "./Output_tasksharing_progression_ucx_patched/results/Elastic-timestep-times.csv" "./Output_sharing_server_bf_patched_ucx_direct/results/Elastic-timestep-times.csv" "./Output_sharing_server_bf_patched_ucx_handshake/results/Elastic-timestep-times.csv"  "./Output_tasksharing_no_progression_ucx_patched/results/Elastic-timestep-times.csv")
-outputFiles=("./plots/data_2_r_2_nodes_base"  "./plots/data_2_r_2_nodes_progression" "./plots/data_2_r_2_nodes_bluefield_direct" "./plots/data_2_r_2_nodes_bluefield_handshake"  "./plots/data_2_r_2_nodes_no_progression" ) 
+inputFiles=("./Output_base_two_nodes/results/Elastic-timestep-times.csv" "./Output_sharing_progress_thread_two_nodes/results/Elastic-timestep-times.csv" "./Output_sharing_server_bf_two_nodes/results/Elastic-timestep-times.csv"  "./Output_sharing_no_progress_two_nodes/results/Elastic-timestep-times.csv")
+outputFiles=("./plots/data_2_r_2_nodes_base"  "./plots/data_2_r_2_nodes_progression" "./plots/data_2_r_2_nodes_bluefield_direct"  "./plots/data_2_r_2_nodes_no_progression" ) 
+
+#inputFiles=("./Output_baseline_ucx_patched_lto/results/Elastic-timestep-times.csv" "./Output_tasksharing_progression_ucx_patched/results/Elastic-timestep-times.csv" "./Output_sharing_server_bf_patched_ucx_direct/results/Elastic-timestep-times.csv" "./Output_sharing_server_bf_patched_ucx_handshake/results/Elastic-timestep-times.csv"  "./Output_tasksharing_no_progression_ucx_patched/results/Elastic-timestep-times.csv")
+#outputFiles=("./plots/data_2_r_2_nodes_base"  "./plots/data_2_r_2_nodes_progression" "./plots/data_2_r_2_nodes_bluefield_direct" "./plots/data_2_r_2_nodes_bluefield_handshake"  "./plots/data_2_r_2_nodes_no_progression" ) 
 
 orders=( 7 8 9 )
 
