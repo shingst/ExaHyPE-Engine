0b1671fbc8a9992fef682b0b58b3ed858c4b0110
Philipp Samfass <philipp.samfass@tum.de>
2021-07-23 14:59:51 +0200

    improved extraction script

--- Miscellaneous/teaMPISoftResilience/extract_data_errors.py
+++ Miscellaneous/teaMPISoftResilience/extract_data_errors.py
@@ -7,6 +7,8 @@ import re
 import os
 import math
 
+import distutils.util
+
 # 220.799197   [i01r01c03s10.sng.lrz.de],rank:0, core:0, tid:0 info         exahype::runners::Runner::printTimeStepInfo(...)        step 11	team = 0	t_min          =0.009801
 timestep_pattern=re.compile("( [0-9]*\.[0-9]*).*step.*team\ =\ ([0-9]).*t_min.*")
 
@@ -25,17 +27,22 @@ error_detect_pattern=re.compile("( [0-9]*\.[0-9]*).*ADERDGSolver::checkCellDescr
 # 4.364132     [login03],rank:0, core:7, tid:0 error        exahype::solvers::ADERDGSolver::correctWithOutcome()    Corrected an error in STP. (file:/dss/dsshome1/02/di57zoh3/Codes/ExaHyPE-Engine/./ExaHyPE/exahype/solvers/ADERDGSolver.cpp,line:3826)
 error_correct_pattern=re.compile("( [0-9]*\.[0-9]*).*ADERDGSolver::correctWithOutcome.*Corrected an error in STP")
 
+#sweep/parameters={"order": "5", "maximumMeshSize": "400", "maximumMeshDepth": "0", "kernels": "generic", "soft_error_generation": "migratable_stp_tasks_overwrite", "check_mechanism": "check_stps_with_low_confidence", "save_redundancy": "true", "task_sharing_mode": "task_sharing_resilience_correction", "check_time_steps": "true", "check_derivatives": "true", "check_admissibility": "true", "check_lazily": "true", "max_error_indicator_derivatives": "0", "max_error_indicator_timestepsizes": "0.03", "offloading": "none", "offloading_progress": "none", "offloading_CCP_temperature": "0.5", "offloading_diffusion_temperature": "0.5", "offloading_CCP_frequency": "0", "offloading_CCP_steps": "0", "offloading_update_temperature": "true", "offloading_increase_temp_threshold": "0", "abs_error": "-1", "rel_error": "0", "injection_rank": "0", "architecture": "skx", "dimension": "3", "bufferSize": "64", "timeSteps": "10", "timeStepping": "global"}
+configuration_pattern=re.compile("sweep/parameters.*\"check_time_steps\": \"(true|false)\".*\"check_derivatives\": \"(true|false)\".*\"check_admissibility\": \"(true|false)\".*\"check_lazily\": \"(true|false)\".*")
+
 #errors = [-1e-7,-1e-6,-1e-5,-1e-4,-1e-3,-1e-2,-1e-1,-1e0,1e0,1e-1,1e-2,1e-3,1e-4,1e-5,1e-6,1e-7]
 errors = [-1e-7,-1e-6,-1e-5,-1e-4,-1e-3,-1e-2,-1e-1,-1e0,-1e1,-1e2,-1e3,-1e4,1e4,1e3,1e2,1e1,1e0,1e-1,1e-2,1e-3,1e-4,1e-5,1e-6,1e-7]
 
 throw_away_results_at_run = 100
 
-def parseErrorFile(filename, teams):
-  #dict={}
-  #for i in range(teams):
+def parseResult(filenamePrefix, teams):
   res={
-      "old" : 0,
-      "new" : 0,
+      "old_value" : 0,
+      "new_value" : 0,
+      "check_adm" : False,
+      "check_der" : False,
+      "check_ts" : False,
+      "check_lazy": False,
       "error_indicator_der" : 0,
       "error_indicator_ts" : 0,
       "error" : 0,
@@ -45,21 +52,18 @@ def parseErrorFile(filename, teams):
       "max_err_ind_ts" : 0,
       "run_number" : 0
   }
-  print (filename)
-  file = open(filename,"r")
-  for line in file:
-    #if "overwriteDouble" in line:
-    #print (line)
+
+  #parse error file
+  errFilename = filenamePrefix+".err"
+  #print(errFilename)
+  err_file = open(errFilename,"r")
+  for line in err_file:
     m = re.match(error_overwrite_pattern, line)
     if m:
-     #print (line)
      res["old"] = float(m.group(2))
      res["new"] =float(m.group(3))
      rel_error = float(m.group(4))
      abs_error = float(m.group(5))
-     #if(rel_error>0):
-     #  error = rel_error
-     #else:
      error = abs_error
      res["error"] = math.copysign(1,error) * 10 **math.ceil(math.log10(abs(error)))
      res["max_err_ind_der"] = float(m.group(6))
@@ -77,88 +81,108 @@ def parseErrorFile(filename, teams):
      res["errors_corrected"] +=1
 
   res["run_number"] = int(filename.split("-")[-1][1:-4])
-  #if(res["errors_corrected"]==1 and res["error"]>0);
-  #  print (filename)
 
-  # additional check
-  if(res["errors_corrected"] == 1):
-    injected = 0
-    healed = 0
-    outfilename = filename[:-3]+"out"
-    outfile = open(outfilename,"r")
-    for line in outfile:
-      m = re.match(stats_pattern, line)
-      if m:
-        injected = int(m.group(2))
-        healed = int(m.group(3))
-    if(injected!=healed):
-      print("ERROR: was corrected on wrong team!")
-      res["errors_corrected"] = 0
- 
+  # parse out file
+  injected = 0
+  healed = 0
+  outFilename = filenamePrefix+".out"
+  outfile = open(outFilename,"r")
+  for line in outfile:
+    m = re.match(stats_pattern, line)
+    if m:
+      injected = int(m.group(2))
+      healed = int(m.group(3))
+    m = re.match(configuration_pattern, line)
+    if m:
+      res["check_ts"] = (bool(distutils.util.strtobool(m.group(1))))
+      res["check_der"] = (bool(distutils.util.strtobool(m.group(2))))
+      res["check_adm"] = (bool(distutils.util.strtobool(m.group(3))))
+      res["check_lazy"] = (bool(distutils.util.strtobool(m.group(4))))
+  if(injected!=healed):
+    print("ERROR: was not corrected appropriately:",outFilename)
+    res["errors_corrected"] = 0
+  elif (res["errors_corrected"]==1):
+    print("Was corrected appropriately:",outFilename)
+
   if(res!=None and res["error"]==0):
-    print("Problem: file ",filename, " does not contain valid output")
-    os.remove(filename)
+    print("Problem: file ",errFilename, " does not contain valid output")
+    os.remove(errFilename)
     return None
 
-  #print(res)
   return res
 
+def accumResultToString(res):
+  if res["runs"]>0:
+    resStr = ""
+    for k in res:
+      resStr += str(res[k])+'\t'
+    resStr += str(res["detected"]/res["runs"])+'\t'
+    resStr += str(res["corrected"]/res["runs"])+'\t'
+    return (resStr)
+  else:
+    return None
 
 def printAccumResults(accumResults):
-  print("max_error_indicator_derivative\tmax_error_indicator_ts\terror\truns\tdetected\tcorrected\tdetection_rate\tcorrection_rate")#\trun_numbers")
+  print("check_adm\tcheck_ts\tcheck_der\tcheck_lazy\tmax_error_indicator_derivative\tmax_error_indicator_ts\terror\truns\tdetected\tcorrected\tdetection_rate\tcorrection_rate")#\trun_numbers")
   for res in accumResults:
-    if res["runs"]>0:
-      print(res["max_err_ind_der"],'\t',res["max_err_ind_ts"],'\t',res["error"],'\t',res["runs"],'\t',res["detected"],'\t',res["corrected"],'\t',res["detected"]/res["runs"],res["corrected"]/res["runs"])#,res["run_numbers"])
+    resstr = accumResultToString(res)
+    if(resstr!=None):
+      print(resstr)
 
- 
-#def sortResults(result_tuples):
-#  result_tuples.sort()
-#  return result_tuples
-#
-def writeResults(accumResults, outputfile):
+def writeAccumResults(accumResults, outputfile):
 #  sortedResults = sortResults(result_tuples)
   f = open(outputfile,"w")
-  f.write("max_error_indicator_derivative\tmax_error_indicator_ts\terror\truns\tdetected\tcorrected\tdetection_rate\tcorrection_rate\n")
+  f.write("check_adm\tcheck_ts\tcheck_der\tcheck_lazy\tmax_error_indicator_derivative\tmax_error_indicator_ts\terror\truns\tdetected\tcorrected\tdetection_rate\tcorrection_rate\n")#\trun_numbers")
   for res in accumResults:
-    if res["runs"]>0:
-      f.write(str(res["max_err_ind_der"])+'\t'+str(res["max_err_ind_ts"])+'\t'+str(res["error"])+'\t'+str(res["runs"])+'\t'+str(res["detected"])+'\t'+str(res["corrected"])+'\t'+str(res["detected"]/res["runs"])+'\t'+str(res["corrected"]/res["runs"])+'\n')
+    resstr = accumResultToString(res)
+    if(resstr!=None):
+      f.write(resstr+"\n")
+  f.close()
 
-def readResults(resultsfile):
+def readAccumResults(resultsfile):
   f = open(resultsfile, "r")
   results = []
   # skip header
   f.readline() 
   for line in f:
     res = line.split('\t')
-    res_dict  = {"max_err_ind_der" : float(res[0]),
-                "max_err_ind_ts" : float(res[1]),
-                "error" : float(res[2]),
-                "detected" : float(res[4]),
-                "corrected" : float(res[5]),
-                "runs" : float(res[3]) }
+    res_dict  = {
+                "check_adm" : bool(distutils.util.strtobool(res[0])), 
+                "check_ts" : bool(distutils.util.strtobool(res[1])), 
+                "check_der" : bool(distutils.util.strtobool(res[2])), 
+                "check_lazy" : bool(distutils.util.strtobool(res[3])), 
+                "max_err_ind_der" : float(res[4]),
+                "max_err_ind_ts" : float(res[5]),
+                "error" : float(res[6]),
+                "runs" : int(res[7]),
+                "detected" : float(res[8]),
+                "corrected" : float(res[9]),
+                }
     results.append(res_dict)
+  f.close()
   return results
 
 def accumulateResults(results):
-  unique_rel_errors = []
-  [unique_rel_errors.append(res["error"]) for res in results if res["error"] not in unique_rel_errors]
-  unique_rel_errors.sort()
+  #unique_rel_errors = []
+  #[unique_rel_errors.append(res["error"]) for res in results if res["error"] not in unique_rel_errors]
+  #unique_rel_errors.sort()
 
-  unique_error_indicators_der = []
-  [unique_error_indicators_der.append(res["max_err_ind_der"]) for res in results if res["max_err_ind_der"] not in unique_error_indicators_der]
-  unique_error_indicators_der.sort()
+  #unique_error_indicators_der = []
+  #[unique_error_indicators_der.append(res["max_err_ind_der"]) for res in results if res["max_err_ind_der"] not in unique_error_indicators_der]
+  #unique_error_indicators_der.sort()
   #print("unique error indicators", unique_error_indicators_der)
   
-  unique_error_indicators_ts = []
-  [unique_error_indicators_ts.append(res["max_err_ind_ts"]) for res in results if res["max_err_ind_ts"] not in  unique_error_indicators_ts]
-  unique_error_indicators_ts.sort()
+  #unique_error_indicators_ts = []
+  #[unique_error_indicators_ts.append(res["max_err_ind_ts"]) for res in results if res["max_err_ind_ts"] not in  unique_error_indicators_ts]
+  #unique_error_indicators_ts.sort()
   #print("unique error indicators", unique_error_indicators_ts)
+
+  unique_configs = [(res["max_err_ind_der"],res["max_err_ind_ts"], res["check_adm"], res["check_ts"], res["check_der"], res["check_lazy"]) for res in results]
+  unique_configs = list(set(unique_configs))
 
   accumResults = []
 
-  for max_error_indicator_der in unique_error_indicators_der:
-    for max_error_indicator_ts in unique_error_indicators_ts:
-      #for rel_error in unique_rel_errors:
+  for config in unique_configs:
       for error in errors:
         runs = 0
         detected = 0
@@ -166,8 +190,9 @@ def accumulateResults(results):
         run_numbers = []
 
         for res in results:
-          if(res["error"]==error and res["max_err_ind_der"]==max_error_indicator_der and res["max_err_ind_ts"]==max_error_indicator_ts):
-            #print("accumulating ", res)
+          if(res["error"]==error and res["max_err_ind_der"]==config[0] and res["max_err_ind_ts"]==config[1] and
+            res["check_adm"]==config[2] and res["check_ts"]==config[3] and res["check_der"]==config[4] and
+            res["check_lazy"]==config[5]):
             runs = runs+1
             if(res["errors_detected"]>0):
                detected=detected+1
@@ -179,52 +204,69 @@ def accumulateResults(results):
             if(runs==throw_away_results_at_run):
               break
         run_numbers.sort()
-        res = {"max_err_ind_der" : max_error_indicator_der,
-               "max_err_ind_ts" : max_error_indicator_ts,
+        res = {"check_adm" : config[2],
+               "check_ts" : config[3],
+               "check_der" : config[4],
+               "check_lazy" : config[5],
+               "max_err_ind_der" : config[0],
+               "max_err_ind_ts" : config[1],
                "error" : error,
+               "runs" : runs,
                "detected" : detected,
                "corrected" : detected_corrected,
-               "runs" : runs#,
     #           "run_numbers" : run_numbers
         }
         if(res["runs"]>0):
-          print (res)
+          #print (res)
           accumResults.append(res)
   return accumResults
 
-def computeAggregateAccuracy(accumResults):
-  #unique_error_indicators_der = []
-  #[unique_error_indicators_der.append(res["max_err_ind_der"]) for res in accumResults if res["max_err_ind_der"] not in unique_error_indicators_der]
-  #unique_error_indicators_der.sort()
-  #print("unique error indicators", unique_error_indicators_der)
-  
-  #unique_error_indicators_ts = []
-  #[unique_error_indicators_ts.append(res["max_err_ind_ts"]) for res in accumResults if res["max_err_ind_ts"] not in  unique_error_indicators_ts]
-  #unique_error_indicators_ts.sort()
-  #print("unique error indicators", unique_error_indicators_ts)
- 
-  error_inds = []
-  error_inds = [(res["max_err_ind_der"],res["max_err_ind_ts"]) for res in accumResults if (res["max_err_ind_der"],res["max_err_ind_ts"]) not in error_inds]
-  error_inds = list(set(error_inds))
+def computeAvgSensitivity(accumResults):
+  unique_configs = [(res["max_err_ind_der"],res["max_err_ind_ts"], res["check_adm"], res["check_ts"], res["check_der"],res["check_lazy"]) for res in accumResults]
+  unique_configs = list(set(unique_configs))
 
+  accuracy_res = []
   accuracy_dict = {}
-  for err_ind in error_inds:
-    accuracy_dict[err_ind] = (0,0)
+  for config in unique_configs:
+    accuracy_dict[config] = (0,0)
 
   for res in accumResults:
-    key = (res["max_err_ind_der"],res["max_err_ind_ts"])
+    key = (res["max_err_ind_der"],res["max_err_ind_ts"],res["check_adm"],res["check_ts"], res["check_der"], res["check_lazy"])
     old_tuple = accuracy_dict[key]
-    print (res)
-    new_tuple = (old_tuple[0]+1, old_tuple[1]+res["corrected"]/res["runs"])
+    #print (res)
+    if(res["runs"]>0):
+      new_tuple = (old_tuple[0]+1, old_tuple[1]+res["corrected"]/res["runs"])
     accuracy_dict[key] = new_tuple
   for key in accuracy_dict:
     cnt = accuracy_dict[key][0]
-    sum_accuracy = accuracy_dict[key][1]
-    accuracy_dict[key] = (cnt,sum_accuracy/cnt)
-  
-  return accuracy_dict
+    if(cnt>0):
+      sum_accuracy = accuracy_dict[key][1]
+      accuracy_dict[key] = (cnt,sum_accuracy/cnt)
+      res_element = {
+			"check_adm" : key[2],
+                     "check_ts" : key[3],
+                     "check_der" : key[4],
+                     "check_lazy" : key[5],
+                     "max_err_ind_der" : key[0],
+                     "max_err_ind_ts" : key[1],
+                     "avg_sensitivity" : sum_accuracy/cnt,
+                     "normalised_realtime_min" : 0,
+                     "cnt" : cnt
+                  }
+      accuracy_res.append(res_element)
+  return accuracy_res
 
-
+def printSensitivityResults(accuracyResults):
+  print("check_adm\tcheck_ts\tcheck_der\tcheck_lazy\tmax_err_ind_der\tmax_err_ind_ts\tavg_sensitivity\tnormalised_realtime_min\ttradeoff_ratio\tcnt")
+  for res in accuracyResults:  
+    if(res["normalised_realtime_min"]>0):
+      factor = res["avg_sensitivity"]/res["normalised_realtime_min"]
+    else:
+      factor = 0
+   
+    print(res["check_adm" ],'\t',res["check_ts" ],\
+         '\t',res["check_der"],'\t',res["check_lazy"],'\t',res["max_err_ind_der"],'\t',res["max_err_ind_ts"],'\t',res["avg_sensitivity"],'\t',res["normalised_realtime_min"],'\t',factor,'\t',res["cnt"])
+    
 if __name__=="__main__":
   dir_base = sys.argv[1]
 
@@ -232,24 +274,20 @@ if __name__=="__main__":
 
   for filename in os.listdir(dir_base):
     if filename.endswith(".err"):
-      #print("reading", filename)
-      result_tuple=parseErrorFile(dir_base+"/"+filename, 2)
+      result_tuple=parseResult(dir_base+"/"+filename[0:-4], 2)
       #print(result_tuple)
       if(result_tuple!=None):
         results += [result_tuple]
 
   results = accumulateResults(results)
-  #print(results)
   printAccumResults(results)
 
-  writeResults(results, "output.txt")
-  results = readResults("output.txt")
+  writeAccumResults(results, "output.txt")
+  results = readAccumResults("output.txt")
 
-  accuracies = computeAggregateAccuracy(results)
+  accuracies = computeAvgSensitivity(results)
 
-  for key in sorted(accuracies):
-    print (key, accuracies[key])
-  ##plt.savefig("error_sensitivity.pdf", bbox_inches='tight', dpi=300)
-  #plt.show()
+  printSensitivityResults(accuracies) 
+
   
 
